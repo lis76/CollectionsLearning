349e9c4957fb54858b139eb272c558e1df8bf7b2
lis76 <rsavoskin@yandex.ru>
2022-12-02 11:00:59 +0300

    Cats Example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Cat[] cats = new Cat[4];
+
+        cats[0] = new Cat("Дым");
+        cats[1] = new Cat("Том");
+        cats[2] = new Cat("Лиса");
+        cats[3] = new Cat("Котяра");
+
+        cats[1] = null;
+
+        System.out.println(Arrays.toString(cats));
     }
+
+
+
 }
