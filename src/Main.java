8dac4a216528cb163fe00e3a8b52719c720bd15d
lis76 <rsavoskin@yandex.ru>
2022-12-12 17:41:01 +0300

    LinkedList Bio example

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Write your code here
+
+        //Arrays
 
         Cat[] cats = new Cat[4];
-
         cats[0] = new Cat("Дым");
         cats[1] = new Cat("Том");
         cats[2] = new Cat("Лиса");
@@ -18,6 +17,7 @@ public class Main {
 
         System.out.println(Arrays.toString(cats));
 
+        // ArrayList
         ArrayList<Cat> catsList = new ArrayList<>();
 
         for (Cat cat : cats){
@@ -41,6 +41,23 @@ public class Main {
         System.out.println(catsList.toString());
 
         System.out.println(catsList.size());
+
+        //LinkedList
+        String str1 = new String("Hello World");
+        String str2 = new String("My name is Roman");
+        String str3 = new String("I love Svetu");
+
+        LinkedList<String> romanBio = new LinkedList<>();
+        romanBio.add(str1);
+        romanBio.add(str2);
+        romanBio.add(str3);
+
+        System.out.println(romanBio);
+
+        romanBio.remove(1);
+
+        System.out.println(romanBio);
+
 
     }
 
